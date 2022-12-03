9d82c8d546bd0a209f05d6bc82790f5c3fff0826
annared <annared1982@gmail.com>
2022-12-03 11:49:47 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -29,6 +29,10 @@ function addCard(gallery) {
 function onImageClick(evt) {
   evt.preventDefault();
 
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
   var lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionPosition: 'bottom',
